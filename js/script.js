77cf672b9719ecec423ec7ccea4d7bebe8da2d42
ellisanderson <ellishenryanderson@gmail.com>
2017-04-07 17:08:09 -0400

    got dice counter working

--- js/script.js
+++ js/script.js
@@ -20,14 +20,19 @@ $(document).ready(function(){
     var diceNumber = moves[turn][2];
     $(cellNumber + " " + playerNumber).css("display", "block");
 
+    document.querySelector('#current-dice-roll p').innerHTML = "You rolled a ";
+    document.querySelector('#current-dice-roll h1').innerHTML = diceNumber;
+
     var lastTurn = turn - 2;
     var lastCellNumber = "#n" + moves[lastTurn][1];
     var lastPlayerNumber = ".player" + moves[lastTurn][0];
     var lastDiceNumber = moves[lastTurn][2];
     $(lastCellNumber + " " + lastPlayerNumber).css("display", "none");
 
-    
   });
+
+
+  
 
 
 
